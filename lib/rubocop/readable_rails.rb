a6a3df93d3f22cfd152cf7994f6086c7997f26c0
Shia <rise.shia@gmail.com>
2021-06-26 06:54:49 +0900

    First commit

---
+++ lib/rubocop/readable_rails.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "readable_rails/version"
+
+module Rubocop
+  module ReadableRails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
